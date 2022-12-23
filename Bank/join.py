09294f3145dc2d63e81ec232416229594f51db33
melih ışık <121313959+hilem3002@users.noreply.github.com>
2022-12-24 00:40:49 +0300

    draft of join def

--- Bank/join.py
+++ Bank/join.py
@@ -1,6 +1,69 @@
 def join(process):
-    process = int(input("lutfen yapmak istediginiz islemi seciniz"))
-    if process == 2:
-        open()
+    # the list of email adresses already exist
+    email_list = []
+    email_txt = open("e-mail.txt", "r")
+    while email_txt.readline() == "":
+        current_email = email_txt.readline()
+        email_list.append(current_email)
+    email_txt.close()
+
+    # key word that are gotta be in an email adress
+    print("you could use these extension: (@gmail.com, @outlook.com, @hotmail.com, @yahoo.com)")
+    print()
+    print("       e-mail examples      ")
+    print("----------------------------")
+    print("   tarkanishere@gmail.com   ")
+    print("    hilem3002@hotmail.com   ")
+    print("  tarkanishere@outlook.com  ")
+    print("     hilem3002@yahoo.com    ")
+    print()
+
+    valid_e_mail_keys = ["@gmail.com", "@outlook.com", "@hotmail.com", "@yahoo.com"]
+    email = input("please enter a e-mail adress")
+
+    # if email does not have the email keys ask for email again
+    is_it_acceptable_email = False
+    while is_it_acceptable_email == False:
+        for e_mail_keys in valid_e_mail_keys:
+            if e_mail_keys in email:
+                is_it_acceptable_email = True
+                break
+            else:
+                is_it_acceptable_email = False
+        if is_it_acceptable_email == False:
+            email = input("this e-mail adress is not valid please select another e-mail adress")
+
+    # if email adress has already used by another user ask for email adress again
+    while email in email_list:
+        email = input("this e-mail adress has already used by another user please select another e-mail adress")
+
+    # we wrote the email adress to the e-mail.txt
+    email_txt = open("email.txt", "a")
+    email_txt.write(f"{email}\n")
+    email_txt.close()
+
+    # requirements that are gotta be in a password
+    print("   your password has to contain these requirements:   ")
+    print("------------------------------------------------------")
+    print("-> lower and upper cases")
+    print("-> at least 6 characters")
+    print("-> at least 1 digit")
+    print()
+    password = input("please enter a password")
+
+    # if password does not have the requirements ask for password again
+    is_it_acceptable_password = False
+    MIN_PASSWORD_LEN = 6
+    while len(password) < MIN_PASSWORD_LEN:
+        password = input("password has to contain at least 6 characters please enter another password")
+    while password.isalpha() == True:
+        password = input("password could not consist of only letters please enter another password")
+    while password.isdigit() == True:
+        password = input("password could not consist of only digits please enter another password")
+    while password.upper() == password:
+        password = input("password could not consist of only upper cases please enter another password")
+    while password.lower() == password:
+        password = input("password could not consist of only lowe cases please enter another password")
+
 
 
