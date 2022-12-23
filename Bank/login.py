c04224540790d53abb2f97de0212473710e81515
Tarkan <99043496+TarkanIsHere@users.noreply.github.com>
2022-12-23 22:40:08 +0300

    Update login.py
    "print("melih")" yazısı kaldırıldı. The print("melih") has been removed.

--- Bank/login.py
+++ Bank/login.py
@@ -3,6 +3,5 @@ def login():
     print("Hello, Welcome to the Jorge Tarkan Jesus Bank!")
     print("1-Sign in \n2-Join Us!")
     process = input("How we can help you?")
-    print("melih")
     return process
     
