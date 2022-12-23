9ab132ac6287326c6345e89885bf71ec4ccc2159
Tarkan <99043496+TarkanIsHere@users.noreply.github.com>
2022-12-23 23:32:52 +0300

    Update login_control.py
    + More features has been added(bug control)

--- Bank/login_control.py
+++ Bank/login_control.py
@@ -1,14 +1,19 @@
 
-
 def login_control(process):
     continue_situation = "E"
+    login_is_correct = False
+    continue_situation_condition_list = ["E", "H"]
     while continue_situation == "E":
         password_correction = False
         email_correction = False
         email = input("Lütfen e-postanızı giriniz:")
+        while "@" not in email or "." not in email or "com" not in email:
+            print("E-mailinizi yanlış formatta girdiniz!")
+            email = input("Lütfen e-postanızı tekrar giriniz:")
         email_txt = open("email.txt", "r")
-        email_list = [0] * 4
         email_counter = -1
+        number_of_customer = 1000
+        email_list = [0] * number_of_customer
         email_opened = email_txt.readline()
         correct_password_index = 0
         while email_opened != "":
@@ -22,7 +27,7 @@ def login_control(process):
                 break
         password = input("Lütfen şifrenizi giriniz:")
         password_txt = open("password.txt", "r")
-        password_list = [0] * 4
+        password_list = [0] * number_of_customer
         password_counter = 0
         password_opened = password_txt.readline()
         while password_opened != "":
@@ -34,10 +39,15 @@ def login_control(process):
                 if correct_password_index == password_list.index(y):
                     password_correction = True
         if password_correction == True and email_correction == True:
-            print("Jorje Tarkan Melih Bankasına Hoşgeldiniz Değerli Üyemiz!")
+            print("Değerli Üyemiz, Jorje Tarkan Melih Bankasına Hoşgeldiniz !")
             print("JTM'li olmak bir ayrıcalıktır!")
-            continue_situation = "h"
+            login_is_correct = True
+            continue_situation = ""
         else:
-            print("şifre veya Epostanız Yanlış!")
+            print("şifre veya e-postanız Yanlış!")
             continue_situation = input("Tekrar Denemek İster Misiniz?(E/H)")
+            while continue_situation not in continue_situation_condition_list:
+                print("Lütfen Sadece 'E' veya 'H' giriniz!")
+                continue_situation = input("Devam etmek istiyorsanız E'yi, istemiyorsanız H'yi tuşlayınız:")
+    return login_is_correct
 
