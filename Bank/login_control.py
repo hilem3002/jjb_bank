54bea6ef46d223e349d420ea28abdff92234f9e3
Tarkan <99043496+TarkanIsHere@users.noreply.github.com>
2022-12-23 22:54:19 +0300

    General Update login_control.py
    +some variable names has been changed.
    +some qualification updates.

--- Bank/login_control.py
+++ Bank/login_control.py
@@ -1,4 +1,5 @@
 
+
 def login_control(process):
     continue_situation = "E"
     while continue_situation == "E":
@@ -7,34 +8,36 @@ def login_control(process):
         email = input("Lütfen e-postanızı giriniz:")
         email_txt = open("email.txt", "r")
         email_list = [0] * 4
-        counter = -1
+        email_counter = -1
         email_opened = email_txt.readline()
         correct_password_index = 0
         while email_opened != "":
-            counter += 1
-            email_list[counter] = email_opened[:-1:]
+            email_counter += 1
+            email_list[email_counter] = email_opened[:-1:]
             email_opened = email_txt.readline()
         for x in email_list:
             if email == x:
                 correct_password_index = email_list.index(x)
                 email_correction = True
                 break
-        sifre = input("Lütfen şifrenizi giriniz:")
-        sifre_txt = open("password.txt", "r")
-        sifre_listesi = [0] * 4
-        sifre_sayaci = 0
-        sifre_okunmus = sifre_txt.readline()
-        while sifre_okunmus != "":
-            sifre_listesi[sifre_sayaci] = sifre_okunmus[:-1:]
-            sifre_sayaci += 1
-            sifre_okunmus = sifre_txt.readline()
-        for y in sifre_listesi:
-            if sifre == y:
-                if correct_password_index == sifre_listesi.index(y):
+        password = input("Lütfen şifrenizi giriniz:")
+        password_txt = open("password.txt", "r")
+        password_list = [0] * 4
+        password_counter = 0
+        password_opened = password_txt.readline()
+        while password_opened != "":
+            password_list[password_counter] = password_opened[:-1:]
+            password_counter += 1
+            password_opened = password_txt.readline()
+        for y in password_list:
+            if password == y:
+                if correct_password_index == password_list.index(y):
                     password_correction = True
         if password_correction == True and email_correction == True:
-            print("Sisteme başarılya girdiniz")
+            print("Jorje Tarkan Melih Bankasına Hoşgeldiniz Değerli Üyemiz!")
+            print("JTM'li olmak bir ayrıcalıktır!")
             continue_situation = "h"
         else:
-            print("Sisteme giremediniz, şifre veya epostanız yanlış!")
-            continue_situation = input("Tekrar denemek ister misiniz?(E/H)")
+            print("şifre veya Epostanız Yanlış!")
+            continue_situation = input("Tekrar Denemek İster Misiniz?(E/H)")
+
